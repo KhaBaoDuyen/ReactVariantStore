b9b7b351aee32c5601f850e7d9e47b4619d81b45
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2026-01-03 22:09:34 +0700

    add slide new blog for BlogPage

--- app/pages/blog/blog.tsx
+++ app/pages/blog/blog.tsx
@@ -6,14 +6,16 @@ import { Link } from "react-router";
 //COMPONENT
 import { Search } from "~/components/UI/Search";
 import { SwipeCategoriesSlide } from "~/components/UI/SwipeCategoriesSlide/SwipeCategoriesSlide";
+import { SwiperBlogSlide } from "~/components/UI/SwiperBlogSlide/SwiperBlogSlide";
 
 
 //DATA
 import { CATEGOGY_DATA } from "~/data/categories.data";
+import { BLOG_DATA } from "~/data/blogs.data";
 
 export default function BlogPage() {
     return (
-        <div>  
+        <div>
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: -25 }}
@@ -23,7 +25,7 @@ export default function BlogPage() {
       lg:rounded-4xl  shadow-lg flex-col gap-2">
                         <h1 className="text-accent-600 font-bold  lg:text-[2rem] text-[1.3rem]">Tin tức</h1>
                         <h1 className="font-bold text-center px-2 title-lg-primary-600 lg:text-[3rem] text-[2rem]">
-                           Khám phá tin tức mới nhất
+                            Khám phá tin tức mới nhất
                         </h1>
                         <span className="lg:w-7/12 w-10/12 bg-white/20 p-3 rounded-md shadow-lg">
                             <Search showOnMobile={true} width="w-12/12" />
@@ -32,12 +34,19 @@ export default function BlogPage() {
                     </div>
                 </div>
             </motion.div>
-             <div className=" mx-auto w-11/12 py-6 lg:w-10/12">
-                      <SwipeCategoriesSlide
-                        data={CATEGOGY_DATA}
-                        type="image"
-                        variant="six" />
-                    </div>
+            <div className=" mx-auto w-11/12 py-6 lg:w-10/12">
+                <SwipeCategoriesSlide
+                    data={CATEGOGY_DATA}
+                    type="image"
+                    variant="six" />
+            </div>
+
+            <section className="lg:w-10/12 mx-auto w-11/12 lg:mt-5">
+                <h1 className="text-[2rem] font-bold text-accent-600 text-center">Tin tức mới</h1>
+                <p className="text-muted text-center">Những bài viết vừa được đăng tải từ đội ngũ của chúng tôi.</p>
+                <SwiperBlogSlide blogs={BLOG_DATA}/>
+
+            </section>
         </div>
     )
 }
